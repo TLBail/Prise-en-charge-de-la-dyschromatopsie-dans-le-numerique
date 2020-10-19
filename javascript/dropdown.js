5db4282a1d0bca1450a0fdf89eb7b561586aaa22
Pollart Nathan <nathan.pollart@hotmail.fr>
2020-10-19 13:37:00 +0200

    modif JS

--- javascript/dropdown.js
+++ javascript/dropdown.js
@@ -1,10 +1,15 @@
 var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
+
 for (i = 0; i < dropdown.length; i++) {
     dropdown[i].addEventListener("click", function() {
+        var sidebarHover = document.getElementsByClassName("navbar")
         this.classList.toggle("active");
         var dropdownContent = this.nextElementSibling;
+        while (sidebarHover.getElementsByClassName().) {
+
+        }
         if (dropdownContent.style.display === "block") {
             dropdownContent.style.display = "none";
         } else {
