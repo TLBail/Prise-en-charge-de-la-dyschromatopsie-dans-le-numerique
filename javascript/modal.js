6f0d3c21715cd5a114fb120e3dedf7ecd996d82a
Pollart Nathan <nathan.pollart@hotmail.fr>
2020-11-03 12:51:22 +0100

    modif pop up

--- javascript/modal.js
+++ javascript/modal.js
@@ -15,17 +15,19 @@ btn.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  document.getElementById("modal_test").style.backgroundImage = "url(../image/test8demi3.jpg)"
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    document.getElementById("modal_test").style.backgroundImage = "url(../image/test8demi3.jpg)"
   }
 }
 
 window.onload = function () {
-    if (localStorage.getItem("hasCodeRunBefore") === null) {
+     if(localStorage.getItem("hasCodeRunBefore") === null) {
         modal.style.display = "block";
         localStorage.setItem("hasCodeRunBefore", true);
     }
@@ -33,5 +35,40 @@ window.onload = function () {
 
 function myFunction(){
 	var answer = document.getElementById("answer").value;
-	document.getElementById("test").innerHTML = answer
+    document.getElementById("modal_test").style.backgroundImage = "url(../image/25.png)"
+    var answer2 = document.getElementById("answer").value;
+
+    if(answer === 8){
+        if(answer2 === 96){
+            //vision normale
+            modal.style.display = "none";
+            document.getElementById("modal_test").style.backgroundImage = "url(../image/test8demi3.jpg)"
+        }
+        else{
+            if(answer2 === 9){
+                //deficience rouge-bleu
+                modal.style.display = "none";
+                document.getElementById("modal_test").style.backgroundImage = "url(../image/test8demi3.jpg)"
+            }
+            else{
+                if(answer2 === 6){
+                //deficience vert-bleu
+                modal.style.display = "none";
+                document.getElementById("modal_test").style.backgroundImage = "url(../image/test8demi3.jpg)"
+                }
+                else{
+                    //vision normale
+                    modal.style.display = "none";
+                    document.getElementById("modal_test").style.backgroundImage = "url(../image/test8demi3.jpg)"
+                }
+            }
+        }
+    }
+    else{
+        if(answer === 3){
+            //deficience rouge-vert
+            modal.style.display = "none";
+            document.getElementById("modal_test").style.backgroundImage = "url(../image/test8demi3.jpg)"
+        }
+    }
 }
