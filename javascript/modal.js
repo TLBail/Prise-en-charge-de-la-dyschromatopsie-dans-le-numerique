cf1cae81f43a4ea1a530cd358ca73aef86401ced
Pollart Nathan <nathan.pollart@hotmail.fr>
2020-11-03 11:11:47 +0100

    javascript modal ajout

---
+++ javascript/modal.js
@@ -0,0 +1,37 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+window.onload = function () {
+    if (localStorage.getItem("hasCodeRunBefore") === null) {
+        modal.style.display = "block";
+        localStorage.setItem("hasCodeRunBefore", true);
+    }
+}
+
+function myFunction(){
+	var answer = document.getElementById("answer").value;
+	document.getElementById("test").innerHTML = answer
+}
